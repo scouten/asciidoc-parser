260dcf77f2babd1735cfe2d19db5489cbc85c686
Eric Scouten <git@scouten.me>
2023-12-08 19:17:52 -0800

    Tighten warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+#![deny(clippy::expect_used)]
+#![deny(clippy::panic)]
+#![deny(clippy::unwrap_used)]
+#![deny(missing_docs)]
+#![deny(warnings)]
+#![doc = include_str!("../README.md")]
+
+/// TEMPORARY demo function
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
