03bbe78dcfc71ed1e5f5c050df53d6021a8a9d82
Eric Scouten <git@scouten.me>
2023-12-16 16:03:24 -0800

    Test coverage

--- src/tests/input.rs
+++ src/tests/input.rs
@@ -9,6 +9,7 @@ mod location {
         let l: Location = (&i).into();
         assert_eq!(l.line(), 1);
         assert_eq!(l.col(), 1);
+        assert_eq!(l.to_string(), "1:1".to_owned());
     }
 
     #[test]
@@ -18,6 +19,7 @@ mod location {
         let l2: Location = (&i2).into();
         assert_eq!(l2.line(), 1);
         assert_eq!(l2.col(), 4);
+        assert_eq!(l2.to_string(), "1:4".to_owned());
     }
 }
 
@@ -49,5 +51,17 @@ mod span {
         assert_eq!(s.after_end().line(), 1);
         assert_eq!(s.after_end().col(), 4);
         assert!(!s.is_empty());
+    }
+
+    #[test]
+    fn impl_eq() {
+        let i = Input::new("", true);
+        let s1 = Span::from_start_and_after_end(&i, &i);
+
+        let i1 = Input::new("abc456", true);
+        let (i2, _) = alpha1::<Input, crate::Error>(i1).unwrap();
+        let s2 = Span::from_start_and_after_end(&i1, &i2);
+
+        assert!(s1 != s2);
     }
 }
