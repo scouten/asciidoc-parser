f16747fbc27cd2e75774dbd6ae5c92a75915daaf
Eric Scouten <git@scouten.me>
2023-12-28 13:29:58 -0800

    Spec-driven development
    Start quotuing the Asciidoc language description(*) and writing tests to track compliance line-by-line.
    
    (* Yes, technically, they don't call it a spec as of yet, but it's as close as we have at the moment and it's quite thorough.)

---
+++ src/tests/asciidoc_lang/mod.rs
@@ -0,0 +1,23 @@
+//! This module quotes from the AsciiDoc language documentation
+//! with the intent of verifying, line-by-line, compliance with
+//! that documentation.
+//!
+//! The quoted documentation can be found in rendered form here:
+//! https://docs.asciidoctor.org/asciidoc/latest/
+//!
+//! and in source form here:
+//! https://gitlab.eclipse.org/eclipse/asciidoc-lang/asciidoc-lang/-/tree/main/docs/modules
+//!
+//! This documentation comes with the following license information:
+//!
+//! > AsciiDoc documentation (c) by Sarah White, Dan Allen, and other
+//! > contributors.
+//!
+//! > Content under this directory is made available under the terms of a
+//! > Creative Commons Attribution 4.0 International License.
+//! > You can find the complete text of this license at https://creativecommons.org/licenses/by/4.0/.
+//!
+//! The submodules in this module are named to match the
+//! page structure as rendered on the AsciiDoctor site.
+
+mod root;
