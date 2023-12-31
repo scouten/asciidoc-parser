3586036fdef4139e5978a02bba85945e879935a0
Eric Scouten <git@scouten.me>
2023-12-30 20:14:57 -0800

    Oh yeah, this should panic ...

--- src/tests/error.rs
+++ src/tests/error.rs
@@ -1,29 +1,12 @@
-use crate::{tests::fixtures::TSpan, Span};
+use crate::Span;
 
 #[test]
+#[should_panic]
 fn incomplete() {
     // Silly test to cover the incomplete case.
 
     let incomplete: nom::Err<nom::error::Error<Span<'static>>> =
-        nom::Err::Error(nom::error::Error {
-            input: Span::new("abc", true),
-            code: nom::error::ErrorKind::NonEmpty,
-        });
+        nom::Err::Incomplete(nom::Needed::Unknown);
 
-    let e: crate::Error<'static> = incomplete.into();
-
-    // Convert to `if let` when we add other error kinds.
-    let crate::Error::NomError(ne) = e;
-
-    assert_eq!(
-        ne.input,
-        TSpan {
-            data: "abc",
-            line: 1,
-            col: 1,
-            offset: 0,
-        }
-    );
-
-    assert_eq!(ne.code, nom::error::ErrorKind::NonEmpty);
+    let _panic: crate::Error<'static> = incomplete.into();
 }
