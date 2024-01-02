b68533285e06b9e714ab4e1e86371f5761d7c769
Eric Scouten <git@scouten.me>
2024-01-02 15:35:42 -0800

    Initial parser for block macros (#33)

---
+++ src/blocks/macro.rs
@@ -0,0 +1,88 @@
+use nom::{
+    bytes::complete::{tag, take_until1},
+    error::{Error, ErrorKind},
+    Err, IResult,
+};
+
+use crate::{
+    primitives::{consume_empty_lines, normalized_line, trim_input_for_rem},
+    HasSpan, Span,
+};
+
+/// A macro block can be used in a block context to create a new block element.
+///
+/// This struct is returned when the block form of a *named macro* is detected.
+///
+/// ```ignore
+/// <name>::<target>?[<attrlist>?].
+/// ```
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct MacroBlock<'a> {
+    name: Span<'a>,
+    target: Option<Span<'a>>,
+    attrlist: Option<Span<'a>>,
+    source: Span<'a>,
+}
+
+impl<'a> MacroBlock<'a> {
+    #[allow(dead_code)]
+    pub(crate) fn parse(source: Span<'a>) -> IResult<Span, Self> {
+        let (rem, line) = normalized_line(source)?;
+
+        let (line, name) = take_until1("::")(line)?;
+        let (line, _) = tag("::")(line)?;
+
+        let (line, target) = if line.starts_with('[') {
+            (line, None)
+        } else {
+            let (line, target) = take_until1("[")(line)?;
+            (line, Some(target))
+        };
+
+        let (line, _) = tag("[")(line)?;
+
+        let (line, attrlist) = if line.starts_with(']') {
+            (line, None)
+        } else {
+            let (line, attrlist) = take_until1("]")(line)?;
+            (line, Some(attrlist))
+        };
+
+        let (line, _) = tag("]")(line)?;
+        if !line.is_empty() {
+            return Err(Err::Error(Error::new(line, ErrorKind::NonEmpty)));
+        }
+
+        let source = trim_input_for_rem(source, rem);
+        Ok((
+            consume_empty_lines(rem),
+            Self {
+                name,
+                target,
+                attrlist,
+                source,
+            },
+        ))
+    }
+
+    /// Return a [`Span`] describing the macro name.
+    pub fn name(&'a self) -> &'a Span<'a> {
+        &self.name
+    }
+
+    /// Return a [`Span`] describing the macro target.
+    pub fn target(&'a self) -> Option<&Span<'a>> {
+        self.target.as_ref()
+    }
+
+    /// Return a [`Span`] describing the macro's attribute list.
+    pub fn attrlist(&'a self) -> Option<&Span<'a>> {
+        self.attrlist.as_ref()
+    }
+}
+
+impl<'a> HasSpan<'a> for MacroBlock<'a> {
+    fn span(&'a self) -> &'a Span<'a> {
+        &self.source
+    }
+}
---
+++ src/tests/fixtures/blocks/macro.rs
@@ -0,0 +1,66 @@
+use std::fmt;
+
+use crate::{blocks::MacroBlock, tests::fixtures::TSpan, HasSpan};
+
+#[derive(Eq, PartialEq)]
+pub(crate) struct TMacroBlock {
+    pub name: TSpan,
+    pub target: Option<TSpan>,
+    pub attrlist: Option<TSpan>,
+    pub source: TSpan,
+}
+
+impl fmt::Debug for TMacroBlock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("MacroBlock")
+            .field("name", &self.name)
+            .field("target", &self.target)
+            .field("attrlist", &self.attrlist)
+            .field("source", &self.source)
+            .finish()
+    }
+}
+
+impl<'a> PartialEq<MacroBlock<'a>> for TMacroBlock {
+    fn eq(&self, other: &MacroBlock<'a>) -> bool {
+        tmacro_block_eq(self, other)
+    }
+}
+
+impl<'a> PartialEq<TMacroBlock> for MacroBlock<'a> {
+    fn eq(&self, other: &TMacroBlock) -> bool {
+        tmacro_block_eq(other, self)
+    }
+}
+
+fn tmacro_block_eq(tmacro_block: &TMacroBlock, macro_block: &MacroBlock) -> bool {
+    if &tmacro_block.name != macro_block.name() {
+        return false;
+    }
+
+    if tmacro_block.target.is_some() != macro_block.target().is_some() {
+        return false;
+    }
+
+    if let Some(ref th_target) = tmacro_block.target {
+        if let Some(ref h_target) = macro_block.target() {
+            if &th_target != h_target {
+                return false;
+            }
+        }
+    }
+
+    if tmacro_block.attrlist.is_some() != macro_block.attrlist().is_some() {
+        return false;
+    }
+
+    if let Some(ref th_attrlist) = tmacro_block.attrlist {
+        if let Some(ref h_attrlist) = macro_block.attrlist() {
+            if &th_attrlist != h_attrlist {
+                return false;
+            }
+        }
+    }
+
+    &tmacro_block.source == macro_block.span()
+}
