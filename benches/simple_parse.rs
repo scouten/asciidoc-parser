082c155594f2ef66d4341b672fd0e9630247e7a6
Eric Scouten <git@scouten.me>
2024-01-02 17:03:34 -0800

    WIP on codspeed integration
    (Lacking bandwidth, so skipping this for now …)

--- benches/simple_parse.rs
+++ benches/simple_parse.rs
@@ -1,5 +1,5 @@
 use asciidoc_parser::Document;
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use codspeed_criterion_compat::{black_box, criterion_group, criterion_main, Criterion};
 
 const TWO_BLOCKS_AND_TITLE: &str = "= Example Title\n\nabc\n\ndef";
 
