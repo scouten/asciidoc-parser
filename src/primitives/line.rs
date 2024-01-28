c1fce5b7cbeec5834a6112f9010e4b872091393c
Eric Scouten <git@scouten.me>
2024-01-28 14:40:38 -0800

    Optimize non_empty_line for the case where there is no line ending

--- src/primitives/line.rs
+++ src/primitives/line.rs
@@ -7,6 +7,7 @@ use nom::{
     Err, IResult, Parser, Slice,
 };
 
+use super::trim_input_for_rem;
 use crate::Span;
 
 /// Return a single line from the source.
@@ -45,6 +46,18 @@ pub(crate) fn normalized_line(input: Span<'_>) -> IResult<Span, Span> {
 /// Returns an error if the line becomes empty after trailing spaces have been
 /// removed.
 pub(crate) fn non_empty_line(input: Span<'_>) -> IResult<Span, Span> {
+    if !input.contains('\n') {
+        let rem = input.slice(input.len()..);
+
+        let (_, line) = trim_trailing_spaces((trim_input_for_rem(input, input), input));
+
+        if line.is_empty() {
+            return Err(Err::Error(Error::new(input, ErrorKind::TakeTill1)));
+        } else {
+            return Ok((rem, line));
+        }
+    }
+
     take_till1(|c| c == '\n')(input)
         .map(|ri| trim_rem_start_matches(ri, '\n'))
         .map(|ri| trim_rem_end_matches(ri, '\r'))
