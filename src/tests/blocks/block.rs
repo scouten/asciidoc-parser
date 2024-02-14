e638c1eb20b831be77b956e0082b51605794b21f
Eric Scouten <git@scouten.me>
2024-02-13 21:20:19 -0700

    Clippy wisely points out that we can just copy here

--- src/tests/blocks/block.rs
+++ src/tests/blocks/block.rs
@@ -542,10 +542,10 @@ mod content_model {
     use crate::blocks::ContentModel;
 
     #[test]
-    fn impl_clone() {
+    fn impl_copy() {
         // Silly test to mark the #[derive(...)] line as covered.
         let c1 = ContentModel::Simple;
-        let c2 = c1.clone();
+        let c2 = c1;
         assert_eq!(c1, c2);
     }
 }
