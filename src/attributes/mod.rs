43a427968c150cac27be4fed13b2d4015d9ecfdc
Eric Scouten <git@scouten.me>
2024-03-02 15:58:10 -0800

    Test fixtures for Attrlist

--- src/attributes/mod.rs
+++ src/attributes/mod.rs
@@ -3,8 +3,8 @@
 //! also be used to add supplemental information, such as citation metadata and
 //! fallback content, to certain elements.
 
-mod element_attribute;
-pub use element_attribute::ElementAttribute;
-
 mod attrlist;
 pub use attrlist::Attrlist;
+
+mod element_attribute;
+pub use element_attribute::ElementAttribute;
