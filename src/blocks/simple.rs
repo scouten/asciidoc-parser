58688498e911e48ec3f6a02b35eee5af388cd54d
Eric Scouten <git@scouten.me>
2024-04-04 21:46:12 -0700

    Change Inline::parse and ::parse_lines to return an Option (#76)

--- src/blocks/simple.rs
+++ src/blocks/simple.rs
@@ -10,7 +10,9 @@ pub struct SimpleBlock<'a>(Inline<'a>);
 
 impl<'a> SimpleBlock<'a> {
     pub(crate) fn parse(source: Span<'a>) -> IResult<Span, Self> {
-        let (rem, inline) = Inline::parse_lines(source)?;
+        let (rem, inline) = Inline::parse_lines(source).ok_or(nom::Err::Error(
+            nom::error::Error::new(source, nom::error::ErrorKind::TakeTill1),
+        ))?;
         Ok((consume_empty_lines(rem), Self(inline)))
     }
 
