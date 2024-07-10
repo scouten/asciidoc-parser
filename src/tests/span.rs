059779c8ae94433f6043feb53db7374ce4252ec6
Eric Scouten <git@scouten.me>
2024-07-09 21:38:24 -0700

    Refine handling of AsRef, AsBytes, and Compare

--- src/tests/span.rs
+++ src/tests/span.rs
@@ -10,15 +10,39 @@ fn test_happy_case() {
 }
 
 #[test]
-fn impl_as_bytes() {
+fn impl_as_ref() {
+    let span = Span::new("abcdef");
+    assert_eq!(span.as_ref(), "abcdef");
+}
+
+#[test]
+fn impl_nom_as_bytes() {
     use nom::AsBytes;
 
     let span = Span::new("abcdef");
     assert_eq!(span.as_bytes(), b"abcdef");
 }
 
-#[test]
-fn impl_as_ref() {
-    let span = Span::new("abcdef");
-    assert_eq!(span.as_ref(), "abcdef");
+mod impl_nom_compare_no_case {
+    use nom::{Compare, CompareResult};
+
+    use crate::Span;
+
+    #[test]
+    fn eq_different_case() {
+        let span = Span::new("BCD");
+        assert_eq!(span.compare_no_case("bcd"), CompareResult::Ok);
+    }
+
+    #[test]
+    fn eq_smae_case() {
+        let span = Span::new("BCD");
+        assert_eq!(span.compare_no_case("BCD"), CompareResult::Ok);
+    }
+
+    #[test]
+    fn error_not_same() {
+        let span = Span::new("BCD");
+        assert_eq!(span.compare_no_case("ABCE"), CompareResult::Error);
+    }
 }
