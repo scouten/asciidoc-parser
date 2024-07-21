5db5627233bb6c4d351dac6bf047fd866f4af5d2
Eric Scouten <git@scouten.me>
2024-07-21 15:36:31 -0700

    Add coverage for crate::Error::from_error_kind

--- src/tests/error.rs
+++ src/tests/error.rs
@@ -10,3 +10,14 @@ fn incomplete() {
 
     let _panic: crate::Error<'static> = incomplete.into();
 }
+
+#[test]
+fn from_error_kind() {
+    use nom::error::ParseError;
+
+    let span = Span::new("abc");
+    let _err = crate::Error::from_error_kind(span, nom::error::ErrorKind::Eof);
+    // nom's error types don't implement Eq, so … 🤷🏻‍♂️
+    // This test case is likely temporary while we explore dropping
+    // the dependency on nom altogether.
+}
