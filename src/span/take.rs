462256ed031849472310251ad7b6313250d51635
Eric Scouten <git@scouten.me>
2024-07-24 22:29:15 -0700

    Add new method `Span::take_while` (#94)

---
+++ src/span/take.rs
@@ -0,0 +1,19 @@
+use nom::InputIter;
+
+use super::{ParseResult, Span};
+
+impl<'a> Span<'a> {
+    /// Split this span at the first character that doesn't match `predicate`.
+    ///
+    /// NOM REFACTOR: Replacement for `is_not`.
+    #[allow(dead_code)] // TEMPORARY
+    pub(crate) fn take_while<P>(self, predicate: P) -> ParseResult<'a, Self>
+    where
+        P: Fn(char) -> bool,
+    {
+        match self.data.position(|c| !predicate(c)) {
+            Some(n) => self.into_parse_result(n),
+            None => self.into_parse_result(self.data.len()),
+        }
+    }
+}
