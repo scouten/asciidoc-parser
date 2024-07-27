4a807ebdaad75967a60afd6d43fc395d880f754b
Eric Scouten <git@scouten.me>
2024-07-26 21:01:03 -0700

    Add `Span::take_prefix` which will replace nom's `tag`

--- src/span/take.rs
+++ src/span/take.rs
@@ -3,6 +3,21 @@ use nom::InputIter;
 use super::{ParseResult, Span};
 
 impl<'a> Span<'a> {
+    /// Split this span, consuming the exact character sequence `prefix` if
+    /// found.
+    ///
+    /// Returns `None` if `prefix` is not found.
+    ///
+    /// NOM REFACTOR: Replacement for `tag`.
+    #[allow(dead_code)]
+    pub(crate) fn take_prefix(self, prefix: &str) -> Option<ParseResult<'a, Self>> {
+        if self.data.starts_with(prefix) {
+            Some(self.into_parse_result(prefix.len()))
+        } else {
+            None
+        }
+    }
+
     /// Split this span at the first character that doesn't match `predicate`.
     ///
     /// NOM REFACTOR: Replacement for `is_not`.
