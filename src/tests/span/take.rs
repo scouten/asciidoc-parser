8b23f023ff2baa4124259e9eb7770c00763a02c1
Eric Scouten <git@scouten.me>
2024-07-26 22:19:46 -0700

    Add new API function `Span::take_whitespace` which will replace `space0`

--- src/tests/span/take.rs
+++ src/tests/span/take.rs
@@ -72,6 +72,114 @@ mod take_prefix {
     }
 }
 
+mod take_whitespace {
+    use crate::{tests::fixtures::TSpan, Span};
+
+    #[test]
+    fn empty_input() {
+        let span = Span::new("");
+        let pr = span.take_whitespace();
+
+        assert_eq!(
+            pr.t,
+            TSpan {
+                data: "",
+                line: 1,
+                col: 1,
+                offset: 0,
+            }
+        );
+
+        assert_eq!(
+            pr.rem,
+            TSpan {
+                data: "",
+                line: 1,
+                col: 1,
+                offset: 0,
+            }
+        );
+    }
+
+    #[test]
+    fn immediate_false() {
+        let s = Span::new(":abc");
+        let pr = s.take_whitespace();
+
+        assert_eq!(
+            pr.t,
+            TSpan {
+                data: "",
+                line: 1,
+                col: 1,
+                offset: 0,
+            }
+        );
+
+        assert_eq!(
+            pr.rem,
+            TSpan {
+                data: ":abc",
+                line: 1,
+                col: 1,
+                offset: 0,
+            }
+        );
+    }
+
+    #[test]
+    fn match_after_first() {
+        let s = Span::new(" \t:cd");
+        let pr = s.take_whitespace();
+
+        assert_eq!(
+            pr.t,
+            TSpan {
+                data: " \t",
+                line: 1,
+                col: 1,
+                offset: 0,
+            }
+        );
+
+        assert_eq!(
+            pr.rem,
+            TSpan {
+                data: ":cd",
+                line: 1,
+                col: 3,
+                offset: 2,
+            }
+        );
+    }
+
+    #[test]
+    fn all_whitespace() {
+        let s = Span::new("  \t ");
+        let pr = s.take_whitespace();
+
+        assert_eq!(
+            pr.t,
+            TSpan {
+                data: "  \t ",
+                line: 1,
+                col: 1,
+                offset: 0,
+            }
+        );
+
+        assert_eq!(
+            pr.rem,
+            TSpan {
+                data: "",
+                line: 1,
+                col: 5,
+                offset: 4,
+            }
+        );
+    }
+}
+
 mod take_while {
     use crate::{tests::fixtures::TSpan, Span};
 
