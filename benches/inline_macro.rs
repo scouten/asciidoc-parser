3e6989acf668c9768678307fe2c848ee9cf2ca9b
Eric Scouten <git@scouten.me>
2024-09-11 19:58:49 -0700

    Maybe we can't use a constant here?

--- benches/inline_macro.rs
+++ benches/inline_macro.rs
@@ -1,11 +1,10 @@
 use asciidoc_parser::Document;
 use codspeed_criterion_compat::{black_box, criterion_group, criterion_main, Criterion};
 
-const BENCH_NAME: &str = "inline macro";
 const PARSE_TEXT: &str = "= Example Title\n\nblah foo:bar[blah] bonus";
 
 pub fn perf(c: &mut Criterion) {
-    c.bench_function(BENCH_NAME, |b| {
+    c.bench_function("inline macro", |b| {
         b.iter(|| Document::parse(black_box(PARSE_TEXT)))
     });
 }
