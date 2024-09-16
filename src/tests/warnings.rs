85920d996dd42fd1f58336d59c82f1eeea75835e
Eric Scouten <git@scouten.me>
2024-09-15 17:30:53 -0700

    Test coverage for `unwrap_if_no_warnings`

--- src/tests/warnings.rs
+++ src/tests/warnings.rs
@@ -59,4 +59,30 @@ mod match_and_warnings {
         let maw2 = maw1.clone();
         assert_eq!(maw1, maw2);
     }
+
+    #[test]
+    fn unwrap_if_no_warnings() {
+        let maw = MatchAndWarnings {
+            item: "xyz",
+            warnings: vec![],
+        };
+
+        let item = maw.unwrap_if_no_warnings();
+        assert_eq!(item, "xyz");
+    }
+
+    #[test]
+    #[should_panic]
+    fn unwrap_if_no_warnings_panic() {
+        let maw = MatchAndWarnings {
+            item: "xyz",
+            warnings: vec![Warning {
+                source: Span::new("abc"),
+                warning: WarningType::EmptyAttributeValue,
+            }],
+        };
+
+        let _ = maw.unwrap_if_no_warnings();
+        // There are warnings so this should panic.
+    }
 }
