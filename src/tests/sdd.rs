f4fd9d671006db871b926914038fd4ce7f93ea21
Eric Scouten <git@scouten.me>
2024-12-01 14:00:07 -0600

    Fix decl macro syntax

--- src/tests/sdd.rs
+++ src/tests/sdd.rs
@@ -3,17 +3,17 @@
 // These macros are read by the parser in scouten/asciidoc-parser-coverage.
 
 // Use the track_file macro to indicate which .adoc spec file is being tracked.
-macro_rules! track_file( ($($tt:tt)*) => {{}} );
+macro_rules! track_file( ($($tt:tt)*) => {} );
 pub(super) use track_file;
 
 // Use the non_normative macro to signal blocks within the .adoc file that are
 // non-normative (i.e. do not describe specific rules that must be obeyed).
-macro_rules! non_normative( ($($tt:tt)*) => {{}} );
+macro_rules! non_normative( ($($tt:tt)*) => {} );
 pub(super) use non_normative;
 
 // Use the verifies_spec macro to annotate a test block that verifies a specific
 // section within the .adoc file that is normative.
-// macro_rules! verifies_spec( ($($tt:tt)*) => {{}} );
+// macro_rules! verifies_spec( ($($tt:tt)*) => {} );
 // pub(super) use verifies_spec;
 
 // All lines in each .adoc file should be covered by either non_normative or
