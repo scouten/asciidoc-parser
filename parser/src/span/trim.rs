860d60db9bf52ee034482ee8e59046e0dc9abac3
Eric Scouten <git@scouten.me>
2025-04-27 07:35:18 -0700

    chore: Add `trim_trailing_line_end` to `Span`

--- parser/src/span/trim.rs
+++ parser/src/span/trim.rs
@@ -7,4 +7,17 @@ impl Span<'_> {
         let new_len = self.data.trim_end().len();
         self.slice(0..new_len)
     }
+
+    #[allow(unused)] // TEMPORARY
+    /// Return a [`Span`] that is the same as the source, but a single trailing
+    /// line-ending (if found) removed.
+    pub(crate) fn trim_trailing_line_end(&self) -> Self {
+        if self.ends_with("\r\n") {
+            self.slice(0..self.len() - 2)
+        } else if self.ends_with('\n') {
+            self.slice(0..self.len() - 1)
+        } else {
+            *self
+        }
+    }
 }
