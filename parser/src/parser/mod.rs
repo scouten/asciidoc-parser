173052bf3d0aea5264e6ef1d95e5383606f93784
Eric Scouten <git@scouten.me>
2025-04-29 19:42:58 -0700

    Silence warnings about unexpected cfg values

--- parser/src/parser/mod.rs
+++ parser/src/parser/mod.rs
@@ -1,5 +1,6 @@
 //! The [`Parser`] struct and its related structs allow a caller to configure
 //! how AsciiDoc parsing occurs and then to initiate the parsing process.
+#![cfg_attr(coverage_nightly, feature(coverage_attribute))]
 
 mod attribute_value;
 pub(crate) use attribute_value::AttributeValue;
@@ -8,11 +9,13 @@ pub use attribute_value::{AllowableValue, ModificationContext};
 mod parser;
 pub use parser::Parser;
 
+#[cfg(test)]
+#[cfg_attr(coverage_nightly, coverage(off))]
 mod tests {
     #[test]
     fn what_happens() {
         assert_eq!(2 + 2, 4);
-        
+
         if false {
             assert_eq!(2 + 2, 5);
         }
