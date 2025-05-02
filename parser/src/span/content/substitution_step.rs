5aa418f92eb986ce32b0d306e9aba6088157e087
Eric Scouten <git@scouten.me>
2025-05-02 12:02:22 -0700

    Very naive proof-of-concept implementation of quotes substitution

--- parser/src/span/content/substitution_step.rs
+++ parser/src/span/content/substitution_step.rs
@@ -1,3 +1,7 @@
+use std::{borrow::Cow, sync::LazyLock};
+
+use regex::{Captures, Regex, Replacer};
+
 use crate::span::Content;
 
 /// Each substitution type replaces characters, markup, attribute references,
@@ -39,6 +43,11 @@ impl SubstitutionStep {
             Self::SpecialCharacters => {
                 apply_special_characters(content);
             }
+
+            Self::Quotes => {
+                apply_quotes(content);
+            }
+
             _ => {
                 todo!("Implement apply for {self:?}");
             }
@@ -60,3 +69,151 @@ fn apply_special_characters(content: &mut Content<'_>) {
 
     content.rendered = new_rendered.into();
 }
+
+static QUOTED_TEXT_SNIFF: LazyLock<Regex> = LazyLock::new(|| Regex::new("[*_`#^~]").unwrap());
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+enum QuoteSubType {
+    Strong,
+    DoubleQuote,
+    SingleQuote,
+    Monospaced,
+    Emphasis,
+    Mark,
+    Superscript,
+    Subscript,
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+enum QuoteSubScope {
+    Constrained,
+    Unconstrained,
+}
+
+struct QuoteSub {
+    type_: QuoteSubType,
+    scope: QuoteSubScope,
+    pattern: Regex,
+}
+
+const QUOTE_ATTR_LIST_RXT: &str = "\\[([^\\[\\]]+)\\]";
+
+// Adapted from QUOTE_SUBS in Ruby Asciidoctor implementation,
+// found in https://github.com/asciidoctor/asciidoctor/blob/main/lib/asciidoctor.rb#L440.
+//
+// Translation notes:
+// * The `\m` modifier on Ruby regex means the `.` pattern *can* match a new
+//   line. This appears to translate to `?m?` in Rust regex syntax.
+// * The `(?!#{CG_WORD})` look-ahead syntax is not available in Rust regex. It
+//   looks like the `\b{end-half}` pattern can take its place. (This pattern
+//   requires that a non-word character or end of haystack follow the match
+//   point.)
+static QUOTE_SUBS: LazyLock<Vec<QuoteSub>> = LazyLock::new(|| {
+    vec![QuoteSub {
+        type_: QuoteSubType::Strong,
+        scope: QuoteSubScope::Constrained,
+        pattern: Regex::new("\\b{start-half}\\*(\\S|\\S.*?\\S)\\*\\b{end-half}").unwrap(),
+        // NOTE: Removed (?:#{QuoteAttributeListRxt})? to bootstrap
+        //       [:strong, :constrained,
+        // /(^|[^#{CC_WORD};:}])(?:#{QuoteAttributeListRxt})?\*(\S|\S#{CC_ALL}*?\S)\*(?!#{CG_WORD})/
+        // m],
+
+        // pattern: Regex::new(&format!("\\?(?:{QUOTE_ATTR_LIST_RXT})?\\*\\*(.+?)\\*\\*")).unwrap(),
+    }]
+});
+
+#[derive(Debug)]
+struct QuoteReplacer {
+    type_: QuoteSubType,
+    scope: QuoteSubScope,
+}
+
+impl Replacer for QuoteReplacer {
+    fn replace_append(&mut self, caps: &Captures<'_>, dst: &mut String) {
+        // Adaapted from Asciidoctor#convert_quoted_text, found in
+        // https://github.com/asciidoctor/asciidoctor/blob/main/lib/asciidoctor/substitutors.rb#L1419-L1445.
+
+        dbg!(&self);
+        dbg!(caps);
+        dbg!(&dst);
+
+        // dst.push_str(&caps["first"]);
+        // dst.push_str(" ");
+        // dst.push_str(&caps["last"]);
+
+        // if match[0].start_with? RS
+        //     if scope == :constrained && (attrs = match[2])
+        //         unescaped_attrs = %([#{attrs}])
+        //     else
+        //         return match[0].slice 1, match[0].length
+        //     end
+        // end
+
+        match self.scope {
+            QuoteSubScope::Constrained => {
+                if false {
+                    todo!(
+                        "{}",
+                        r##"
+                        if unescaped_attrs
+                            %(#{unescaped_attrs}#{Inline.new(self, :quoted, match[3], type: type).convert})
+                        else
+                            if (attrlist = match[2])
+                            id = (attributes = parse_quoted_text_attributes attrlist)['id']
+                            type = :unquoted if type == :mark
+                            end
+                            %(#{match[1]}#{Inline.new(self, :quoted, match[3], type: type, id: id, attributes: attributes).convert})
+                        end
+                    "##
+                    );
+                }
+
+                // TEMPORARY: POC with simplest possible implementation for now.
+                dst.push_str("<em>");
+                dst.push_str(&caps[1]);
+                dst.push_str("</em>");
+            }
+
+            QuoteSubScope::Unconstrained => {
+                todo!(
+                    r#"
+                    if (attrlist = match[1])
+                        id = (attributes = parse_quoted_text_attributes attrlist)['id']
+                        type = :unquoted if type == :mark
+                    end
+                        Inline.new(self, :quoted, match[2], type: type, id: id, attributes: attributes).convert
+                    end
+"#
+                );
+            }
+        }
+    }
+}
+
+fn apply_quotes(content: &mut Content<'_>) {
+    if !QUOTED_TEXT_SNIFF.is_match(content.rendered.as_ref()) {
+        eprintln!("QT sniff said no match");
+        return;
+    }
+
+    let mut result: Cow<'_, str> = content.rendered.to_string().into();
+
+    for sub in &*QUOTE_SUBS {
+        let replacer = QuoteReplacer {
+            type_: sub.type_,
+            scope: sub.scope,
+        };
+
+        dbg!(&replacer);
+
+        if let Cow::Owned(new_result) = sub.pattern.replace_all(&result, replacer) {
+            result = new_result.into();
+        }
+        // If it's Cow::Borrowed, there was no match for this pattern, so no
+        // need to pay for a new string allocation.
+    }
+
+    content.rendered = result.into();
+}
+
+// https://github.com/asciidoctor/asciidoctor/blob/main/lib/asciidoctor/substitutors.rb#L1419-L1445
--- parser/src/tests/span/content/substitution_step.rs
+++ parser/src/tests/span/content/substitution_step.rs
@@ -29,6 +29,48 @@ mod special_characters {
     }
 }
 
+mod quotes {
+    use crate::{span::content::SubstitutionStep, strings::CowStr, Content, Span};
+
+    #[test]
+    fn empty() {
+        let mut content = Content::from(Span::new(""));
+        SubstitutionStep::Quotes.apply(&mut content);
+        assert!(content.is_empty());
+        assert_eq!(content.rendered, CowStr::Borrowed(""));
+    }
+
+    #[test]
+    fn basic_non_empty_span() {
+        let mut content = Content::from(Span::new("blah"));
+        SubstitutionStep::Quotes.apply(&mut content);
+        assert!(!content.is_empty());
+        assert_eq!(content.rendered, CowStr::Borrowed("blah"));
+    }
+
+    #[test]
+    fn ignore_lt_and_gt() {
+        let mut content = Content::from(Span::new("bl<ah>"));
+        SubstitutionStep::Quotes.apply(&mut content);
+        assert!(!content.is_empty());
+        assert_eq!(
+            content.rendered,
+            CowStr::Boxed("bl<ah>".to_string().into_boxed_str())
+        );
+    }
+
+    #[test]
+    fn strong_word() {
+        let mut content = Content::from(Span::new("One *word* is strong."));
+        SubstitutionStep::Quotes.apply(&mut content);
+        assert!(!content.is_empty());
+        assert_eq!(
+            content.rendered,
+            CowStr::Boxed("One <em>word</em> is strong.".to_string().into_boxed_str())
+        );
+    }
+}
+
 mod callouts {
     use crate::{span::content::SubstitutionStep, strings::CowStr, Content, Span};
 
