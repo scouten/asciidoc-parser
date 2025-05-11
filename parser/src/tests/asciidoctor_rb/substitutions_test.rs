1f12ab17cb2493a4e23c8e141225823d16fa7bc8
Eric Scouten <git@scouten.me>
2025-05-10 17:41:06 -0700

    Handle escaped single-line unconstrained emphasized chars

--- parser/src/tests/asciidoctor_rb/substitutions_test.rs
+++ parser/src/tests/asciidoctor_rb/substitutions_test.rs
@@ -723,17 +723,22 @@ mod quotes {
         assert_eq!(content.rendered, CowStr::Borrowed("<em>Git</em>Hub"));
     }
 
+
+    #[test]
+    fn escaped_single_line_unconstrained_emphasized_characters() {
+        let mut content = Content::from(Span::new(r#"\__Git__Hub"#));
+        let r = HtmlSubstitutionRenderer {};
+        SubstitutionStep::Quotes.apply(&mut content, &r);
+        assert!(!content.is_empty());
+        assert_eq!(content.rendered, CowStr::Borrowed("__Git__Hub"));
+    }
+
     #[ignore]
     #[test]
     fn todo_migrate_from_ruby() {
         todo!(
             "{}",
             r###"
-    test 'escaped single-line unconstrained emphasized chars' do
-      para = block_from_string %(#{BACKSLASH}__Git__Hub)
-      assert_equal '__Git__Hub', para.sub_quotes(para.source)
-    end
-
     test 'escaped single-line unconstrained emphasized chars around word' do
       para = block_from_string %(#{BACKSLASH}#{BACKSLASH}__GitHub__)
       assert_equal '__GitHub__', para.sub_quotes(para.source)
