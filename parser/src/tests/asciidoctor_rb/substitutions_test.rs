f8ad6523fa8a359a655942167907bb8340ace046
Eric Scouten <git@scouten.me>
2025-05-12 20:30:38 -0700

    Port test 'quoted text with id and role shorthand with roles before id'

--- parser/src/tests/asciidoctor_rb/substitutions_test.rs
+++ parser/src/tests/asciidoctor_rb/substitutions_test.rs
@@ -1132,17 +1132,24 @@ mod quotes {
         );
     }
 
+    #[test]
+    fn quoted_text_with_id_and_role_shorthand_with_roles_before_id() {
+        let mut content = Content::from(Span::new("[.white.red-background#bond]#007#"));
+        let r = HtmlSubstitutionRenderer {};
+        SubstitutionStep::Quotes.apply(&mut content, &r);
+        assert!(!content.is_empty());
+        assert_eq!(
+            content.rendered,
+            CowStr::Borrowed(r#"<span id="bond" class="white red-background">007</span>"#)
+        );
+    }
+
     #[ignore]
     #[test]
     fn todo_migrate_from_ruby() {
         todo!(
             "{}",
             r###"
-    test 'quoted text with id and role shorthand with roles before id' do
-      para = block_from_string '[.white.red-background#bond]#007#'
-      assert_equal '<span id="bond" class="white red-background">007</span>', para.sub_quotes(para.source)
-    end
-
     test 'quoted text with id and role shorthand with roles around id' do
       para = block_from_string '[.white#bond.red-background]#007#'
       assert_equal '<span id="bond" class="white red-background">007</span>', para.sub_quotes(para.source)
