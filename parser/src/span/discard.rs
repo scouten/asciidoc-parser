f36b409ca23c2594142414605ec04b524a3f44bb
Eric Scouten <git@scouten.me>
2025-05-18 16:56:00 -0700

    chore: Add `Span::discard_whitespace()`

--- parser/src/span/discard.rs
+++ parser/src/span/discard.rs
@@ -7,6 +7,13 @@ impl Span<'_> {
         self.into_parse_result(n).after
     }
 
+    #[allow(unused)]
+    /// Return a new span, discarding any whitespace found at the beginning of
+    /// the input span.
+    pub(crate) fn discard_whitespace(self) -> Self {
+        self.take_whitespace().after
+    }
+
     /// Return a new span, discarding all characters in the input span.
     pub(crate) fn discard_all(self) -> Self {
         self.discard(self.len())
