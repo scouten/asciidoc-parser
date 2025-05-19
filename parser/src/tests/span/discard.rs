cbe51e47ae4ab263fb5bdf5137ca9af796a61b1b
Eric Scouten <git@scouten.me>
2025-05-18 17:02:14 -0700

    chore: Add `Span::discard_whitespace()` (#247)

--- parser/src/tests/span/discard.rs
+++ parser/src/tests/span/discard.rs
@@ -72,6 +72,80 @@ mod discard {
     }
 }
 
+mod discard_whitespace {
+    use crate::{tests::fixtures::TSpan, Span};
+
+    #[test]
+    fn empty_source() {
+        let span = Span::new("");
+        assert_eq!(
+            span.discard_whitespace(),
+            TSpan {
+                data: "",
+                line: 1,
+                col: 1,
+                offset: 0,
+            }
+        );
+    }
+
+    #[test]
+    fn all_whitespace() {
+        let span = Span::new("   \t  ");
+        assert_eq!(
+            span.discard_whitespace(),
+            TSpan {
+                data: "",
+                line: 1,
+                col: 7,
+                offset: 6,
+            }
+        );
+    }
+
+    #[test]
+    fn doesnt_consume_newline() {
+        let span = Span::new("   \nabc");
+        assert_eq!(
+            span.discard_whitespace(),
+            TSpan {
+                data: "\nabc",
+                line: 1,
+                col: 4,
+                offset: 3,
+            }
+        );
+    }
+
+    #[test]
+    fn some_whitespace() {
+        let span = Span::new("   abc");
+        assert_eq!(
+            span.discard_whitespace(),
+            TSpan {
+                data: "abc",
+                line: 1,
+                col: 4,
+                offset: 3,
+            }
+        );
+    }
+
+    #[test]
+    fn no_whitespace() {
+        let span = Span::new("abc");
+        assert_eq!(
+            span.discard_whitespace(),
+            TSpan {
+                data: "abc",
+                line: 1,
+                col: 1,
+                offset: 0,
+            }
+        );
+    }
+}
+
 mod discard_all {
     use crate::{tests::fixtures::TSpan, Span};
 
