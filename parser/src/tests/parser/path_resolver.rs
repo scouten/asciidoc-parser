2f618a9d3d9ba2b129c57a8d5d6c8590450e730c
Eric Scouten <git@scouten.me>
2025-07-06 07:01:08 -0700

    Clippy

--- parser/src/tests/parser/path_resolver.rs
+++ parser/src/tests/parser/path_resolver.rs
@@ -3,22 +3,28 @@ mod posixify {
 
     #[test]
     fn replaces_backslashes_if_windowsish() {
-        let mut pr = PathResolver::default();
-        pr.file_separator = '\\';
+        let pr = PathResolver {
+            file_separator: '\\',
+        };
+
         assert_eq!(pr.posixify("abc/def\\ghi"), "abc/def/ghi");
     }
 
     #[test]
     fn doesnt_replace_backslashes_if_posixish() {
-        let mut pr = PathResolver::default();
-        pr.file_separator = '/';
+        let pr = PathResolver {
+            file_separator: '/',
+        };
+
         assert_eq!(pr.posixify("abc/def\\ghi"), "abc/def\\ghi");
     }
 
     #[test]
     fn doesnt_replace_backslashes_if_none_exist() {
-        let mut pr = PathResolver::default();
-        pr.file_separator = '\\';
+        let pr = PathResolver {
+            file_separator: '\\',
+        };
+
         assert_eq!(pr.posixify("abc/def"), "abc/def");
     }
 }
