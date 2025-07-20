b890dce86e4569cf6e5396f505159c187a21ee3e
Eric Scouten <git@scouten.me>
2025-07-20 16:03:57 -0700

    chore: Implement regex::replace_all but with a minimal lookahead implementation

---
+++ parser/src/tests/internal/regex.rs
@@ -0,0 +1,74 @@
+use regex::{Captures, Regex, RegexBuilder};
+
+use crate::internal::{LookaheadReplacer, LookaheadResult, replace_with_lookahead};
+
+#[test]
+fn example() {
+    let re = Regex::new(r"(?m)^(\S+)[\s--\r\n]+(\S+)$").unwrap();
+
+    let hay = "
+Greetings  1973
+Wild\t1973
+BornToRun\t\t\t\t1975
+Darkness                    1978
+TheRiver 1980
+";
+
+    let new = replace_with_lookahead(&re, hay, "$2 $1");
+
+    assert_eq!(
+        new,
+        "
+1973 Greetings
+1973 Wild
+1975 BornToRun
+1978 Darkness
+1980 TheRiver
+"
+    );
+}
+
+impl<'a> LookaheadReplacer for &'a str {
+    fn replace_append(
+        &mut self,
+        caps: &Captures<'_>,
+        dst: &mut String,
+        _after: &str,
+    ) -> LookaheadResult {
+        caps.expand(*self, dst);
+        LookaheadResult::Continue
+    }
+}
+
+#[test]
+fn match_after_quote() {
+    let re = RegexBuilder::new(r#"(^|[^\w&;:"'`}])(?:\[([^\[\]]+)\])?`(\S|\S.*?\S)`\b{end-half}"#)
+        .dot_matches_new_line(true)
+        .build()
+        .unwrap();
+
+    let hay = "Olaf had been with the company since the `'00s.\nHis desk overflowed with heaps of paper, apple cores and squeaky toys.\nWe couldn't find Olaf's keyboard.\nThe state of his desk was replicated, in triplicate, across all of\nthe werewolves`' desks.";
+
+    let new = replace_with_lookahead(&re, hay, Rejectifier {});
+
+    assert_eq!(
+        new,
+        "Olaf had been with the company since the `'HELLO00s.\nHis desk overflowed with heaps of paper, apple cores and squeaky toys.\nWe couldn't find Olaf's keyboard.\nThe state of his desk was replicated, in triplicate, across all of\nthe werewolves`' desks."
+    );
+}
+
+struct Rejectifier {}
+
+impl LookaheadReplacer for Rejectifier {
+    fn replace_append(
+        &mut self,
+        caps: &Captures<'_>,
+        dst: &mut String,
+        after: &str,
+    ) -> LookaheadResult {
+        assert_eq!(after, "' desks.");
+        dst.push_str(&caps[0][0..3]);
+        dst.push_str("HELLO");
+        LookaheadResult::SkipAheadAndRetry(3)
+    }
+}
