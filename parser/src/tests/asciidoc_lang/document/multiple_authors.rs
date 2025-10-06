72bcb00a284e4e05a8cb580023b4758dfacc9c01
Eric Scouten <git@scouten.me>
2025-10-05 19:46:43 -0700

    Add multiple authors tests with escaping character references
    - Add test for basic multi-author syntax
    - Add test for escaping trailing character references with {empty}
    - Tests currently expose a parser bug where semicolons in character references are incorrectly treated as author separators

---
+++ parser/src/tests/asciidoc_lang/document/multiple_authors.rs
@@ -0,0 +1,189 @@
+use pretty_assertions_sorted::assert_eq;
+
+use crate::{Parser, tests::prelude::*};
+
+track_file!("docs/modules/document/pages/multiple-authors.adoc");
+
+non_normative!(
+    r#"
+= Add Multiple Authors to a Document
+
+The xref:author-line.adoc[author line] is the only way to assign more than one author to a document for display in the byline.
+Additionally, only the HTML 5 and Docbook converters can convert documents with multiple authors.
+
+"#
+);
+
+#[test]
+fn multi_author_syntax() {
+    non_normative!(
+        r#"
+== Multi-author syntax
+
+"#
+    );
+
+    verifies!(
+        r#"
+The information for each author is concluded with a semicolon (`;`).
+
+.Author line structure for multiple authors
+[source]
+----
+= Document Title
+firstname middlename lastname <email>; firstname middlename lastname <email>
+----
+
+Directly after each author's last name or optional email, enter a semicolon (`;`) followed by a space, and then enter the next author's information.
+
+"#
+    );
+
+    let doc = Parser::default().parse("= The Intrepid Chronicles\nKismet R. Lee <kismet@asciidoctor.org>; B. Steppenwolf; Pax Draeke <pax@asciidoctor.org>");
+
+    assert_eq!(
+        doc,
+        Document {
+            header: Header {
+                title_source: Some(Span {
+                    data: "The Intrepid Chronicles",
+                    line: 1,
+                    col: 3,
+                    offset: 2,
+                },),
+                title: Some("The Intrepid Chronicles",),
+                attributes: &[],
+                author_line: Some(AuthorLine {
+                    authors: &[
+                        Author {
+                            name: "Kismet R. Lee",
+                            firstname: "Kismet",
+                            middlename: Some("R.",),
+                            lastname: Some("Lee",),
+                            email: Some("kismet@asciidoctor.org",),
+                        },
+                        Author {
+                            name: "B. Steppenwolf",
+                            firstname: "B.",
+                            middlename: None,
+                            lastname: Some("Steppenwolf",),
+                            email: None,
+                        },
+                        Author {
+                            name: "Pax Draeke",
+                            firstname: "Pax",
+                            middlename: None,
+                            lastname: Some("Draeke",),
+                            email: Some("pax@asciidoctor.org",),
+                        },
+                    ],
+                    source: Span {
+                        data: "Kismet R. Lee <kismet@asciidoctor.org>; B. Steppenwolf; Pax Draeke <pax@asciidoctor.org>",
+                        line: 2,
+                        col: 1,
+                        offset: 26,
+                    },
+                },),
+                revision_line: None,
+                comments: &[],
+                source: Span {
+                    data: "= The Intrepid Chronicles\nKismet R. Lee <kismet@asciidoctor.org>; B. Steppenwolf; Pax Draeke <pax@asciidoctor.org>",
+                    line: 1,
+                    col: 1,
+                    offset: 0,
+                },
+            },
+            blocks: &[],
+            source: Span {
+                data: "= The Intrepid Chronicles\nKismet R. Lee <kismet@asciidoctor.org>; B. Steppenwolf; Pax Draeke <pax@asciidoctor.org>",
+                line: 1,
+                col: 1,
+                offset: 0,
+            },
+            warnings: &[],
+        }
+    );
+}
+
+#[test]
+fn escape_trailing_character_reference() {
+    non_normative!(
+        r#"
+=== Escape a trailing character reference
+
+"#
+    );
+
+    verifies!(
+        r#"
+If an author name segment ends with a character reference (e.g., `\&#174;`), you must escape it from processing.
+One way to escape it is to add a trailing attribute reference (e.g., `\{empty}`).
+If the character reference appears at the end of the last author name segment, you can use a second semicolon instead.
+
+A better way of escaping the character reference is to replace it with an attribute reference (e.g., `\{reg}`).
+
+Even if the character reference is escaped, the segments of the author name will not be processed.
+Instead, the whole name will be assigned to the `author` and `firstname` attributes.
+This limitation may be lifted in the future.
+
+"#
+    );
+
+    let doc = Parser::default().parse("= Document Title\nAsciiDoc&#174;{empty} WG; Another Author");
+
+    assert_eq!(
+        doc,
+        Document {
+            header: Header {
+                title_source: Some(Span {
+                    data: "Document Title",
+                    line: 1,
+                    col: 3,
+                    offset: 2,
+                },),
+                title: Some("Document Title",),
+                attributes: &[],
+                author_line: Some(AuthorLine {
+                    authors: &[
+                        Author {
+                            name: "AsciiDoc&#174;{empty} WG",
+                            firstname: "AsciiDoc&#174;{empty} WG",
+                            middlename: None,
+                            lastname: None,
+                            email: None,
+                        },
+                        Author {
+                            name: "Another Author",
+                            firstname: "Another",
+                            middlename: None,
+                            lastname: Some("Author",),
+                            email: None,
+                        },
+                    ],
+                    source: Span {
+                        data: "AsciiDoc&#174;{empty} WG; Another Author",
+                        line: 2,
+                        col: 1,
+                        offset: 17,
+                    },
+                },),
+                revision_line: None,
+                comments: &[],
+                source: Span {
+                    data: "= Document Title\nAsciiDoc&#174;{empty} WG; Another Author",
+                    line: 1,
+                    col: 1,
+                    offset: 0,
+                },
+            },
+            blocks: &[],
+            source: Span {
+                data: "= Document Title\nAsciiDoc&#174;{empty} WG; Another Author",
+                line: 1,
+                col: 1,
+                offset: 0,
+            },
+            warnings: &[],
+        }
+    );
+}
