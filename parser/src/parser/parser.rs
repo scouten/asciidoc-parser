eaadf3f0789e6de425278eeb3089fc97c175dc6d
Eric Scouten <git@scouten.me>
2025-10-12 22:04:30 -0700

    Tweak whitespace

--- parser/src/parser/parser.rs
+++ parser/src/parser/parser.rs
@@ -252,7 +252,6 @@ impl Parser {
         renderer: ISR,
     ) -> Self {
         self.renderer = Rc::new(renderer);
-
         self
     }
 
@@ -268,7 +267,6 @@ impl Parser {
     /// [`IncludeFileHandler::resolve_target()`]: crate::parser::IncludeFileHandler::resolve_target
     pub fn with_primary_file_name<S: AsRef<str>>(mut self, name: S) -> Self {
         self.primary_file_name = Some(name.as_ref().to_owned());
-
         self
     }
 
@@ -284,7 +282,6 @@ impl Parser {
         handler: IFH,
     ) -> Self {
         self.include_file_handler = Some(Rc::new(handler));
-
         self
     }
 
