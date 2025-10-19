a9efcb2fe4a8e99182e69afd44ab579b7c16186c
Eric Scouten <git@scouten.me>
2025-10-19 13:40:31 -0700

    Remove now-obsolete commentary on `mod test` reason for existence

--- parser/src/tests/mod.rs
+++ parser/src/tests/mod.rs
@@ -1,6 +1,3 @@
-// Tests are grouped under this module so as to avoid
-// having the test code itself included in coverage numbers.
-
 #![allow(clippy::expect_used)]
 #![allow(clippy::panic)]
 #![allow(clippy::unwrap_used)]
