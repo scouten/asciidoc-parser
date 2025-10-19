2fe5e55eaa5dee770b318ca690f69780d7fd2ca8
Eric Scouten <git@scouten.me>
2025-10-19 13:26:54 -0700

    Move `discard` test suite for `Span`

--- parser/src/span/discard.rs
+++ parser/src/span/discard.rs
@@ -18,3 +18,194 @@ impl Span<'_> {
         self.discard(self.len())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    #![allow(clippy::unwrap_used)]
+
+    mod discard {
+        use pretty_assertions_sorted::assert_eq;
+
+        use crate::tests::prelude::*;
+
+        #[test]
+        fn empty_source() {
+            let span = crate::Span::default();
+            assert_eq!(
+                span.discard(4),
+                Span {
+                    data: "",
+                    line: 1,
+                    col: 1,
+                    offset: 0,
+                }
+            );
+        }
+
+        #[test]
+        fn n_gt_len() {
+            let span = crate::Span::new("abc");
+            assert_eq!(
+                span.discard(6),
+                Span {
+                    data: "",
+                    line: 1,
+                    col: 4,
+                    offset: 3,
+                }
+            );
+        }
+
+        #[test]
+        fn n_eq_len() {
+            let span = crate::Span::new("abc");
+            assert_eq!(
+                span.discard(3),
+                Span {
+                    data: "",
+                    line: 1,
+                    col: 4,
+                    offset: 3,
+                }
+            );
+        }
+
+        #[test]
+        fn n_lt_len() {
+            let span = crate::Span::new("abc");
+            assert_eq!(
+                span.discard(2),
+                Span {
+                    data: "c",
+                    line: 1,
+                    col: 3,
+                    offset: 2,
+                }
+            );
+        }
+
+        #[test]
+        fn zero() {
+            let span = crate::Span::new("abc");
+            assert_eq!(
+                span.discard(0),
+                Span {
+                    data: "abc",
+                    line: 1,
+                    col: 1,
+                    offset: 0,
+                }
+            );
+        }
+    }
+
+    mod discard_whitespace {
+        use pretty_assertions_sorted::assert_eq;
+
+        use crate::tests::prelude::*;
+
+        #[test]
+        fn empty_source() {
+            let span = crate::Span::default();
+            assert_eq!(
+                span.discard_whitespace(),
+                Span {
+                    data: "",
+                    line: 1,
+                    col: 1,
+                    offset: 0,
+                }
+            );
+        }
+
+        #[test]
+        fn all_whitespace() {
+            let span = crate::Span::new("   \t  ");
+            assert_eq!(
+                span.discard_whitespace(),
+                Span {
+                    data: "",
+                    line: 1,
+                    col: 7,
+                    offset: 6,
+                }
+            );
+        }
+
+        #[test]
+        fn doesnt_consume_newline() {
+            let span = crate::Span::new("   \nabc");
+            assert_eq!(
+                span.discard_whitespace(),
+                Span {
+                    data: "\nabc",
+                    line: 1,
+                    col: 4,
+                    offset: 3,
+                }
+            );
+        }
+
+        #[test]
+        fn some_whitespace() {
+            let span = crate::Span::new("   abc");
+            assert_eq!(
+                span.discard_whitespace(),
+                Span {
+                    data: "abc",
+                    line: 1,
+                    col: 4,
+                    offset: 3,
+                }
+            );
+        }
+
+        #[test]
+        fn no_whitespace() {
+            let span = crate::Span::new("abc");
+            assert_eq!(
+                span.discard_whitespace(),
+                Span {
+                    data: "abc",
+                    line: 1,
+                    col: 1,
+                    offset: 0,
+                }
+            );
+        }
+    }
+
+    mod discard_all {
+        use pretty_assertions_sorted::assert_eq;
+
+        use crate::tests::prelude::*;
+
+        #[test]
+        fn empty_source() {
+            let span = crate::Span::default();
+            assert_eq!(
+                span.discard_all(),
+                Span {
+                    data: "",
+                    line: 1,
+                    col: 1,
+                    offset: 0,
+                }
+            );
+        }
+
+        #[test]
+        fn non_empty() {
+            let span = crate::Span::new("abc");
+            assert_eq!(
+                span.discard(3),
+                Span {
+                    data: "",
+                    line: 1,
+                    col: 4,
+                    offset: 3,
+                }
+            );
+        }
+    }
+}
--- parser/src/tests/span/mod.rs
+++ parser/src/tests/span/mod.rs
@@ -73,7 +73,6 @@ mod split_at_match_non_empty {
     }
 }
 
-mod discard;
 mod line;
 mod matched_item;
 mod parse_result;
