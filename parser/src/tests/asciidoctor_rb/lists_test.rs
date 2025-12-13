b7b620144b7a3d24204cc55c1fdbd8105111c826
Eric Scouten <git@scouten.me>
2025-12-13 14:24:45 -0800

    Port test 'asterisk elements with interspersed line comments should be skipped and not break list'

--- parser/src/tests/asciidoctor_rb/lists_test.rs
+++ parser/src/tests/asciidoctor_rb/lists_test.rs
@@ -6499,29 +6499,226 @@ mod bulleted_lists {
         }
 
         #[test]
+        fn asterisk_elements_separated_by_a_line_comment_offset_by_blank_lines_should_not_merge_lists()
+         {
+            let doc = Parser::default().parse("* Foo\n* Boo\n\n//\n\n* Blech");
+
+            // NOTE: This test will change when we implement line comment parsing.
+
+            assert_eq!(
+                doc,
+                Document {
+                    header: Header {
+                        title_source: None,
+                        title: None,
+                        attributes: &[],
+                        author_line: None,
+                        revision_line: None,
+                        comments: &[],
+                        source: Span {
+                            data: "",
+                            line: 1,
+                            col: 1,
+                            offset: 0,
+                        },
+                    },
+                    blocks: &[
+                        Block::List(ListBlock {
+                            type_: ListType::Unordered,
+                            items: &[
+                                Block::ListItem(ListItem {
+                                    marker: ListItemMarker::Asterisks(Span {
+                                        data: "*",
+                                        line: 1,
+                                        col: 1,
+                                        offset: 0,
+                                    },),
+                                    blocks: &[Block::Simple(SimpleBlock {
+                                        content: Content {
+                                            original: Span {
+                                                data: "Foo",
+                                                line: 1,
+                                                col: 3,
+                                                offset: 2,
+                                            },
+                                            rendered: "Foo",
+                                        },
+                                        source: Span {
+                                            data: "Foo",
+                                            line: 1,
+                                            col: 3,
+                                            offset: 2,
+                                        },
+                                        style: SimpleBlockStyle::Paragraph,
+                                        title_source: None,
+                                        title: None,
+                                        anchor: None,
+                                        anchor_reftext: None,
+                                        attrlist: None,
+                                    },),],
+                                    source: Span {
+                                        data: "* Foo",
+                                        line: 1,
+                                        col: 1,
+                                        offset: 0,
+                                    },
+                                    anchor: None,
+                                    anchor_reftext: None,
+                                    attrlist: None,
+                                },),
+                                Block::ListItem(ListItem {
+                                    marker: ListItemMarker::Asterisks(Span {
+                                        data: "*",
+                                        line: 2,
+                                        col: 1,
+                                        offset: 6,
+                                    },),
+                                    blocks: &[Block::Simple(SimpleBlock {
+                                        content: Content {
+                                            original: Span {
+                                                data: "Boo",
+                                                line: 2,
+                                                col: 3,
+                                                offset: 8,
+                                            },
+                                            rendered: "Boo",
+                                        },
+                                        source: Span {
+                                            data: "Boo",
+                                            line: 2,
+                                            col: 3,
+                                            offset: 8,
+                                        },
+                                        style: SimpleBlockStyle::Paragraph,
+                                        title_source: None,
+                                        title: None,
+                                        anchor: None,
+                                        anchor_reftext: None,
+                                        attrlist: None,
+                                    },),],
+                                    source: Span {
+                                        data: "* Boo",
+                                        line: 2,
+                                        col: 1,
+                                        offset: 6,
+                                    },
+                                    anchor: None,
+                                    anchor_reftext: None,
+                                    attrlist: None,
+                                },),
+                            ],
+                            source: Span {
+                                data: "* Foo\n* Boo",
+                                line: 1,
+                                col: 1,
+                                offset: 0,
+                            },
+                            title_source: None,
+                            title: None,
+                            anchor: None,
+                            anchor_reftext: None,
+                            attrlist: None,
+                        },),
+                        Block::Simple(SimpleBlock {
+                            content: Content {
+                                original: Span {
+                                    data: "//",
+                                    line: 4,
+                                    col: 1,
+                                    offset: 13,
+                                },
+                                rendered: "",
+                            },
+                            source: Span {
+                                data: "//",
+                                line: 4,
+                                col: 1,
+                                offset: 13,
+                            },
+                            style: SimpleBlockStyle::Paragraph,
+                            title_source: None,
+                            title: None,
+                            anchor: None,
+                            anchor_reftext: None,
+                            attrlist: None,
+                        },),
+                        Block::List(ListBlock {
+                            type_: ListType::Unordered,
+                            items: &[Block::ListItem(ListItem {
+                                marker: ListItemMarker::Asterisks(Span {
+                                    data: "*",
+                                    line: 6,
+                                    col: 1,
+                                    offset: 17,
+                                },),
+                                blocks: &[Block::Simple(SimpleBlock {
+                                    content: Content {
+                                        original: Span {
+                                            data: "Blech",
+                                            line: 6,
+                                            col: 3,
+                                            offset: 19,
+                                        },
+                                        rendered: "Blech",
+                                    },
+                                    source: Span {
+                                        data: "Blech",
+                                        line: 6,
+                                        col: 3,
+                                        offset: 19,
+                                    },
+                                    style: SimpleBlockStyle::Paragraph,
+                                    title_source: None,
+                                    title: None,
+                                    anchor: None,
+                                    anchor_reftext: None,
+                                    attrlist: None,
+                                },),],
+                                source: Span {
+                                    data: "* Blech",
+                                    line: 6,
+                                    col: 1,
+                                    offset: 17,
+                                },
+                                anchor: None,
+                                anchor_reftext: None,
+                                attrlist: None,
+                            },),],
+                            source: Span {
+                                data: "* Blech",
+                                line: 6,
+                                col: 1,
+                                offset: 17,
+                            },
+                            title_source: None,
+                            title: None,
+                            anchor: None,
+                            anchor_reftext: None,
+                            attrlist: None,
+                        },),
+                    ],
+                    source: Span {
+                        data: "* Foo\n* Boo\n\n//\n\n* Blech",
+                        line: 1,
+                        col: 1,
+                        offset: 0,
+                    },
+                    warnings: &[],
+                    source_map: SourceMap(&[]),
+                    catalog: Catalog {
+                        refs: HashMap::from([]),
+                        reftext_to_id: HashMap::from([]),
+                    },
+                }
+            );
+        }
+
+        #[test]
         #[ignore]
         fn port_from_ruby() {
             todo!(
                 "Port this: {}",
                 r###"
-    test 'asterisk elements separated by a line comment offset by blank lines should not merge lists' do
-      input = <<~'EOS'
-      List
-      ====
-
-      * Foo
-      * Boo
-
-      //
-
-      * Blech
-      EOS
-      output = convert_string input
-      assert_xpath '//ul', output, 2
-      assert_xpath '(//ul)[1]/li', output, 2
-      assert_xpath '(//ul)[2]/li', output, 1
-    end
-
     test 'asterisk elements separated by a block title offset by a blank line should not merge lists' do
       input = <<~'EOS'
       List
